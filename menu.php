1f857fcefc74bc7998b144d66427fd24c0b34fcf
ResaDev1 <resamahdiabedi@gmail.com>
2021-10-19 11:39:18 +0330

    Second menu checkbox added

--- menu.php
+++ menu.php
@@ -1,29 +1,82 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $result = 0;
+
+        if (isset($_POST["checkSecondMenu"])) {
+            $result = 1;
+        }
+        else {
+            $result = 0;
+        }
+
+        match ($result) {
+            0 => Db::replace(DB_TABLE, array("id" => 0, "param" => "secondMenu", "value" => "false"), array("%s", "%s")),
+            1 => Db::replace(DB_TABLE, array("id" => 0, "param" => "secondMenu", "value" => "true"), array("%s", "%s")),
+        };
+    }
+
+?>
+
 <html>
     <style>
         .hidden {
             display: none;
         }
+
+		main form {
+		    display: flex;
+		    flex-direction: column;
+            align-items: baseline;
+            margin-top: 1rem;
+		}
+
+        button {
+            background: #a0a0a0;
+            border: none;
+        }
+
+        input[type=submit] {
+            background: none;
+            border: 1px solid #a0a0a0;
+            padding: 6px;
+            cursor: pointer;
+        }
     </style>
 
-    <div>
-        <h1>Twenty Twenty Plus control panel</h1>
+    <main>
+        <h1>Twenty Twenty Two Plus control panel</h1>
         
-        <button onclick="toggleLogs()">Logs</button>
-        
-        <textarea name="Logs" id="Logs" cols="60" rows="10" dir="ltr" class="hidden">
-            <?php
-                $messages = get_logs();
-                foreach ($messages as $message) {
-                    echo $message . "\n";
-                }
-                ?>
-        </textarea>
+        <form method="POST" action="">
+            <div style="padding: 10px; display: flex; align-items: flex-end;">
+                <label for="secondmenu" style="margin-left: 5px;">فهرست دوم</label>
+                <?php 
+                    $results = Db::get("SELECT value FROM wp_tttp WHERE id=0;");
 
-        <form>
-            <label for="theme-color">رنگ پوسته :</label>
-            <input type="text">
+                    echo match ($results[0]->value) {
+                        "true" => '<input type="checkbox" name="checkSecondMenu" value="checked" checked>',
+                        "false" => '<input type="checkbox" name="checkSecondMenu" value="checked">'
+                    };
+                ?>
+            </div>
+
+            <input type="submit" value="ثبت تغییرات" style="margin-top: 10px; margin-bottom: 10px;">
         </form>
-    </div>
+        
+    </main>
+    <hr>
+    <p>نسخه : <?php echo get_plugin_version(); ?></p>
+    <button onclick="toggleLogs()" style="position: fixed; bottom: 2rem;">Logs</button>
+
+    
+    <textarea name="Logs" id="Logs" cols="60" rows="10" dir="ltr" class="hidden">
+        <?php
+            $messages = get_logs();
+            foreach ($messages as $message) {
+                echo $message . "\n";
+            }
+        ?>
+    </textarea>
     <script>
         function toggleLogs() {
             document.getElementById("Logs").classList.toggle('hidden');
