1f51d8ab7fa678b6b51f509bef01a1b506bec9da
ResaDev1 <resamahdiabedi@gmail.com>
2021-10-20 14:41:25 +0330

    New Update. Table, button fixed.

--- menu.php
+++ menu.php
@@ -31,42 +31,80 @@
             margin-top: 1rem;
 		}
 
-        button {
-            background: #a0a0a0;
-            border: none;
-        }
-
         input[type=submit] {
             background: none;
             border: 1px solid #a0a0a0;
             padding: 6px;
             cursor: pointer;
         }
+
+        table {
+            border: solid 1px #a0a0a0;
+            background: #fff;
+        }
+
+        table, td {
+            text-align: center;
+            width: 20rem;
+        }
+
+        th {
+            border-bottom: solid 1px #a0a0a0;
+        }
+
+        .tttp-bottom-section {
+            position: fixed;
+            bottom: 10px;
+        }
+
+        hr {
+            margin-bottom: 2rem;
+            margin-top: 2rem;
+        }
     </style>
 
     <main>
-        <h1>Twenty Twenty Two Plus control panel</h1>
+        <h1>تنظیمات افزونه</h1>
         
         <form method="POST" action="">
-            <div style="padding: 10px; display: flex; align-items: flex-end;">
-                <label for="secondmenu" style="margin-left: 5px;">فهرست دوم</label>
-                <?php 
-                    $results = Db::get("SELECT value FROM wp_tttp WHERE id=0;");
+            <table>
+                <thead>
+                    <tr>
+                        <th>پارامتر</th>
+                        <th>مقدار</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div style="padding: 10px; display: flex; align-items: flex-end;">
+                                <label for="secondmenu">
+                                    فهرست دوم
+                                </label>
+                            </div>
+                        </td>
+                        <td>
+                            <?php 
+                                $results = Db::get("SELECT value FROM wp_tttp WHERE id=0;");
 
-                    echo match ($results[0]->value) {
-                        "true" => '<input type="checkbox" name="checkSecondMenu" value="checked" checked>',
-                        "false" => '<input type="checkbox" name="checkSecondMenu" value="checked">'
-                    };
-                ?>
-            </div>
-
-            <input type="submit" value="ثبت تغییرات" style="margin-top: 10px; margin-bottom: 10px;">
+                                echo match ($results[0]->value) {
+                                    "true" => '<input type="checkbox" name="checkSecondMenu" value="checked" checked>',
+                                    "false" => '<input type="checkbox" name="checkSecondMenu" value="checked">'
+                                };
+                            ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="submit" value="ثبت تغییرات" class="button" style="margin-top: 1rem;">
         </form>
         
     </main>
     <hr>
-    <p>نسخه : <?php echo get_plugin_version(); ?></p>
-    <button onclick="toggleLogs()" style="position: fixed; bottom: 2rem;">Logs</button>
+    <div class="tttp-bottom-section">
+        <p>نسخه : <?php echo get_plugin_version(); ?></p>
+        <button onclick="toggleLogs()">Logs</button>
+    </div>
 
     
     <textarea name="Logs" id="Logs" cols="60" rows="10" dir="ltr" class="hidden">
