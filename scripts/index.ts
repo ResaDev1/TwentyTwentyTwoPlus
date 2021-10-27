89c3c8916e531daf12ee7bf6e98867b734680ef7
ResaDev1 <resamahdiabedi@gmail.com>
2021-10-27 14:40:34 +0330

    Nav handle

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,15 +1,15 @@
 import "../css/style.css";
-import { Switch, ThemeState } from "./switch";
+import { Switch } from "./switch";
 
-const THEME_HEADER_QUERY: string = "body .wp-site-blocks header nav ul";
-
-// Main function
+// Add switch to Header
 document.addEventListener("DOMContentLoaded", () => {
+    const header: Element = document.querySelector(
+        "body .wp-site-blocks header nav ul"
+    );
+
     let switchElement = new Switch({
         body: document.querySelector("body"),
     });
-
-    let header = document.querySelector(THEME_HEADER_QUERY);
 
     // Append switch to header
     header.appendChild(switchElement.getElement);
@@ -18,3 +18,32 @@ document.addEventListener("DOMContentLoaded", () => {
         switchElement.changeStateAuto();
     });
 });
+
+/**
+ * @name handleWindowClick
+ * @description Handle Window click event
+ * @param {Event} event
+ * @returns void
+ */
+function handleWindowClick(event: MouseEvent): void {
+    // First get click taget
+    const target = event.target;
+
+    const nav: Element = document.querySelector("nav .has-modal-open");
+    const body: Element = document.querySelector("body");
+    const html: Element = document.querySelector("html");
+
+    switch (target) {
+        case body:
+            // Close nav
+            html.classList.remove("has-modal-open");
+            nav.classList.remove("is-menu-open");
+            break;
+
+        default:
+            break;
+    }
+}
+
+// On window click
+window.addEventListener("click", handleWindowClick);
