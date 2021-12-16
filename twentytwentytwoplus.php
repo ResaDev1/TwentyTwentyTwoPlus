5d1e123248b4e2850a448c62ec95342a4c930793
ResaDev1 <resamahdiabedi@gmail.com>
2021-12-16 12:21:21 +0330

    Version changed

--- twentytwentytwoplus.php
+++ twentytwentytwoplus.php
@@ -4,7 +4,7 @@
     * Plugin Name: TwentyTwentyTwoPlus
     * Author: The Resasadr dev team
     * Description: TwentyTwentyTwoPlus plugin
-    * Version: 0.1.3
+    * Version: 0.1.3-stable001
     * Requires at least: 5.8
     * License: GNU General Public License v3.0
     * License URI: http://www.gnu.org/licenses/gpl-3.0.html
